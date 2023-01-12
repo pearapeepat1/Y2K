f4343224bb363775ed089f8e86e56bdbb1d71559
duangtawan123 <63209010021@chontech.ac.th>
2023-01-12 11:50:50 +0700

    showMinor

--- navigater.php
+++ navigater.php
@@ -5,13 +5,13 @@
         <a class="nav-link active text-light" href="index.php">Active</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-light" href="show_province.php">Student3</a>
+        <a class="nav-link text-light" href="show_province.php">Province</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-light" href="#">Link</a>
+        <a class="nav-link text-light" href="customer.php">Customer</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled " href="#">Disabled</a>
+        <a class="nav-link disabled " href="minor.php">Minor</a>
       </li>
     </ul>
   </div>
