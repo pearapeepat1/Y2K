cb3bf10a313884dbab8341c5ba26665b152ceb02
pearapeepat1 <peelover25@gmail.com>
2023-01-12 11:40:55 +0700

    edit4

--- navigater.php
+++ navigater.php
@@ -5,10 +5,13 @@
         <a class="nav-link active text-light" href="index.php">Active</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-light" href="show_province.php">Student3</a>
+        <a class="nav-link text-light" href="show_province.php">Province</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-light" href="#">Link</a>
+        <a class="nav-link text-light" href="#">Customer</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light" href="#">Minor</a>
       </li>
       <li class="nav-item">
         <a class="nav-link disabled " href="#">Disabled</a>
