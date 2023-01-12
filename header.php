6b6f00ef0d5f8248553f090dbd2cd4520674cd14
pearapeepat1 <peelover25@gmail.com>
2023-01-12 11:39:02 +0700

    edit3

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Data of Student3</title>
+  <title>Groub name : Y2K</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="bootstrap.min.css" rel="stylesheet">
